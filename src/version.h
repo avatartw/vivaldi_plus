b63d93006c608a9bcc7cc67028ef24954a460b23
GitHub Action <action@github.com>
2024-05-08 11:38:17 +0800

    update version number

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define RELEASE_VER_MAIN 1
 #define RELEASE_VER_SUB 8
 #define RELEASE_VER_FIX 1
-#define RELEASE_VER_PATCH 0
+#define RELEASE_VER_PATCH 1
 
 #define TOSTRING2(arg) #arg
 #define TOSTRING(arg) TOSTRING2(arg)
